e9659f6aaeb490a6bdc9de7e1078747eefc762b2
Nahuel <nahue_cge@hotmail.com>
2025-08-14 14:50:50 -0300

    Add login endpoint to user authentication URLs

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import RegistroUsuarioView
+from dj_rest_auth.views import LoginView
     
 urlpatterns = [
     path('registro/', RegistroUsuarioView.as_view(), name='registro'),
+    path('login/', LoginView.as_view(), name='login')
 ]
